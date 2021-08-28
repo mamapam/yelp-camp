2aa2556daf81eaf03bb796d6a91f5a010b408faa
mamapam <mamapam.dev@gmail.com>
2021-08-28 10:15:44 -0400

    Display reviews on page

--- app.js
+++ app.js
@@ -80,7 +80,9 @@ app.post(
 app.get(
   '/campgrounds/:id',
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate(
+      'reviews'
+    );
     res.render('campgrounds/show', { campground });
   })
 );
