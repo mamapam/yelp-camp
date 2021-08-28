12d6d1795491ae84feb69b11297f9fbb2ddc20b1
mamapam <mamapam.dev@gmail.com>
2021-08-28 09:44:51 -0400

    Created Review model

--- models/campground.js
+++ models/campground.js
@@ -8,6 +8,12 @@ const CampgroundSchema = new Schema({
   price: Number,
   description: String,
   location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review',
+    },
+  ],
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
