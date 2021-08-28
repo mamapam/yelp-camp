1a33255cdaf658a176731fbe4acb3fe8fb450e34
mamapam <mamapam.dev@gmail.com>
2021-08-28 14:40:53 -0400

    Added functionality to add author to camp

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -36,6 +36,7 @@ router.post(
   validateCampground,
   catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`);
@@ -45,9 +46,9 @@ router.post(
 router.get(
   '/:id',
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate(
-      'reviews'
-    );
+    const campground = await Campground.findById(req.params.id)
+      .populate('reviews')
+      .populate('author');
     if (!campground) {
       req.flash('error', 'Cannot find that campground!');
       return res.redirect('/campgrounds');
--- seeds/index.js
+++ seeds/index.js
@@ -28,6 +28,7 @@ const seedDb = async () => {
       description:
         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam placeat, adipisci unde aspernatur officiis alias blanditiis? Impedit vitae, perferendis molestias unde illum rerum eius voluptatum repellat, odit delectus tempore ut?',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      author: '612a7ed6e188a65b9c002cf5',
     });
     await camp.save();
   }
