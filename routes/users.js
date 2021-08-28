b945591f48de7432bff055593733f5097654f950
mamapam <mamapam.dev@gmail.com>
2021-08-28 14:32:42 -0400

    Added returnTo functionality

--- routes/users.js
+++ routes/users.js
@@ -42,7 +42,9 @@ router.post(
   }),
   (req, res) => {
     req.flash('success', 'Welcome Back!');
-    res.redirect('/campgrounds');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
   }
 );
 
