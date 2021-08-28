6c40ace0d005deda9b76f17def69fbc927d6b4ce
mamapam <mamapam.dev@gmail.com>
2021-08-28 14:16:37 -0400

    Added logout functionality

--- routes/users.js
+++ routes/users.js
@@ -41,4 +41,10 @@ router.post(
   }
 );
 
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success', 'Goodbye!');
+  res.redirect('/campgrounds');
+});
+
 module.exports = router;
