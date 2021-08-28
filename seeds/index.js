5eb0de41da7812e9993ce11b854ec813a0e50c51
mamapam <mamapam.dev@gmail.com>
2021-08-28 07:11:01 -0400

    Added images and updated campground model

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,14 @@ const seedDb = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      price,
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam placeat, adipisci unde aspernatur officiis alias blanditiis? Impedit vitae, perferendis molestias unde illum rerum eius voluptatum repellat, odit delectus tempore ut?',
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
     });
     await camp.save();
   }
