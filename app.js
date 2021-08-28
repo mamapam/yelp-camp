97861c27de0be21de4fe1f1c5bb7a3e45cbc8d62
mamapam <mamapam.dev@gmail.com>
2021-08-28 01:38:37 -0400

    Created campgrounds show page

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/show', { campground });
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
 });
