4957a5a8c4db3d2877b0dfcb43cffc2ccbdb3b47
mamapam <mamapam.dev@gmail.com>
2021-08-28 11:38:40 -0400

    Added error flash messages

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -46,6 +46,10 @@ router.get(
     const campground = await Campground.findById(req.params.id).populate(
       'reviews'
     );
+    if (!campground) {
+      req.flash('error', 'Cannot find that campground!');
+      return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground });
   })
 );
@@ -54,6 +58,10 @@ router.get(
   '/:id/edit',
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+      req.flash('error', 'Cannot find that campground!');
+      return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground });
   })
 );
