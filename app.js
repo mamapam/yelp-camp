3f7c902ba802119412d2d2bfc0a62b99b33d3de1
mamapam <mamapam.dev@gmail.com>
2021-08-28 00:26:44 -0400

    Setup starter views

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-  res.send('Hello from Yelp Camp!');
+  res.render('home');
 });
 
 app.listen(3000, () => {
