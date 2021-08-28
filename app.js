0294c8683e6ae001e49b6d908a451e8527524d98
mamapam <mamapam.dev@gmail.com>
2021-08-28 01:01:18 -0400

    Created campground model

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
 
 const app = express();
 
@@ -8,6 +21,15 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/makecampground', async (req, res) => {
+  const camp = new Campground({
+    title: 'My backyard',
+    description: 'Cheap camping!',
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
