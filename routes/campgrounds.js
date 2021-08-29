25ca710198fa0f79ffcee27f3d972025a3934a99
mamapam <mamapam.dev@gmail.com>
2021-08-29 09:45:27 -0400

    Added multer

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,17 +2,23 @@ const express = require('express');
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 const router = express.Router();
 
 router
   .route('/')
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateCampground,
+  //   catchAsync(campgrounds.createCampground)
+  // );
+  .post(upload.array('image'), (req, res) => {
+    console.log(req.body, req.files);
+    res.send('it worked');
+  });
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
