86d6f74f046eeef9e9fef04159b82b71b91a5a19
mamapam <mamapam.dev@gmail.com>
2021-08-29 18:06:56 -0400

    Added upload to cloudinary functionality

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,7 +3,9 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { storage } = require('../cloudinary');
+
+const upload = multer({ storage });
 
 const router = express.Router();
 
