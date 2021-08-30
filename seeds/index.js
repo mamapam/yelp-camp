36fda34186c8af35f06ed4b00100a358e5caedee
mamapam <mamapam.dev@gmail.com>
2021-08-30 05:30:48 -0400

    Fixed image seeds

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
+const Review = require('../models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -18,12 +19,22 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDb = async () => {
   await Campground.deleteMany({});
+  await Review.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/483251',
+      images: [
+        {
+          url: 'https://res.cloudinary.com/dijwegvdc/image/upload/v1630315658/YelpCamp/djrz8mguh8ey7nd56vnh.jpg',
+          filename: 'YelpCamp/djrz8mguh8ey7nd56vnh',
+        },
+        {
+          url: 'https://res.cloudinary.com/dijwegvdc/image/upload/v1630315660/YelpCamp/wg5qzyrcwuo9j4gwgqf0.jpg',
+          filename: 'YelpCamp/wg5qzyrcwuo9j4gwgqf0',
+        },
+      ],
       price,
       description:
         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam placeat, adipisci unde aspernatur officiis alias blanditiis? Impedit vitae, perferendis molestias unde illum rerum eius voluptatum repellat, odit delectus tempore ut?',
