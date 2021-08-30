d209fb666e218d10ad2095218265536a8fd19685
mamapam <mamapam.dev@gmail.com>
2021-08-30 06:40:55 -0400

    Added thumbails to edit page

--- models/campground.js
+++ models/campground.js
@@ -3,14 +3,18 @@ const Review = require('./review');
 
 const { Schema } = mongoose;
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+  return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
   title: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
