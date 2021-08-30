1bfea5dad6e4ac5390b2465f3f615f0280282328
mamapam <mamapam.dev@gmail.com>
2021-08-30 09:48:02 -0400

    Added popups to map

--- models/campground.js
+++ models/campground.js
@@ -12,33 +12,44 @@ ImageSchema.virtual('thumbnail').get(function () {
   return this.url.replace('/upload', '/upload/w_200');
 });
 
-const CampgroundSchema = new Schema({
-  title: String,
-  images: [ImageSchema],
-  price: Number,
-  description: String,
-  location: String,
-  geometry: {
-    type: {
-      type: String,
-      enum: ['Point'],
-      required: true,
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema(
+  {
+    title: String,
+    images: [ImageSchema],
+    price: Number,
+    description: String,
+    location: String,
+    geometry: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  reviews: [
-    {
+    author: {
       type: Schema.Types.ObjectId,
-      ref: 'Review',
+      ref: 'User',
     },
-  ],
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Review',
+      },
+    ],
+  },
+  opts
+);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+  return `
+  <strong><a href="campgrounds/${this._id}">${this.title}</a></strong><p>
+  <p>${this.description.substring(0, 20)}...</p>`;
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
