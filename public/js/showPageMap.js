37c85cb41043f4b3379d68c2cafe2d5e82f82486
mamapam <mamapam.dev@gmail.com>
2021-08-30 07:40:49 -0400

    Show campground on map

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -2,6 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9, // starting zoom
+  center: campground.coordinates, // starting position [lng, lat]
+  zoom: 10, // starting zoom
 });
+
+new mapboxgl.Marker().setLngLat(campground.coordinates).addTo(map);
